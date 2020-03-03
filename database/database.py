2df0e3511e65bd00028a302e40d418481473928b
plutownium <rolandmackintosh@gmail.com>
2020-03-02 21:40:54 -0800

    minor tweak to database.py

--- database/database.py
+++ database/database.py
@@ -280,21 +280,21 @@ def drop_all_tables():
 # *~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~
 
 # NEW on 2020/02/29: Refreshing ALL data in db and adding a few select cities, languages
-drop_all_tables()
+# drop_all_tables()
 
 
 # ### Do a LOT of scraping... Let's see if I get banned... Start time: 6:00 pm
-# langs_to_add = ["vue", "angular", "react", "html", "css", "javascript", "python", "php", "mongodb", "sql"]
-# short_langs = ["vue", "angular", "react"]
-# cities_to_add = ["Vancouver", "Toronto", "Seattle", "New York", "Silicon Valley"]
-# # ### Add the query "lang, loc" to the database:
-# for lang in short_langs:
-#     for city in cities_to_add:
-#         # Add the Query to the db, including its Page Soups and Post objects
-#         query_status = add_plain_query_to_database(Query(lang, city))
-#         # Run update_post_from_soup n times for the query
-#         if query_status:
-#             process_entire_query([lang, city])
+langs_to_add = ["vue", "angular", "react", "html", "css", "javascript", "python", "php", "mongodb", "sql"]
+short_langs = ["vue", "angular", "react"]
+cities_to_add = ["Vancouver", "Toronto", "Seattle", "New York"]
+# ### Add the query "lang, loc" to the database:
+for lang in langs_to_add:
+    for city in cities_to_add:
+        # Add the Query to the db, including its Page Soups and Post objects
+        query_status = add_plain_query_to_database(Query(lang, city))
+        # Run update_post_from_soup n times for the query
+        if query_status:
+            process_entire_query([lang, city])
 
 
 meta = MetaData()
